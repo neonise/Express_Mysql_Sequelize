f78671128cd406cb6ea29866a45b67d7280b25d7
parvizwpf <30858096+parvizwpf@users.noreply.github.com>
2022-09-20 09:27:02 +0430

    init

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const app = express();
 
 let corsOptions = {
-  origin: "http://localhost:8081",
+  origin: ["http://localhost:8081", "http://localhost:5173"],
 };
 
 app.use(cors(corsOptions));
